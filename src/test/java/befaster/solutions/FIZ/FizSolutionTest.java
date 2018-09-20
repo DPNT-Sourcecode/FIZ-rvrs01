c77347b640934d50f7190e7bcbb623bd58505f26
sbx_user1059 <sbx_user1059@ip-10-129-141-175.eu-west-2.compute.internal>
2018-09-20 07:05:38 +0000

    Thu Sep 20 07:05:38 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizSolutionTest.java
@@ -1,4 +1,20 @@
 package befaster.solutions.FIZ;
 
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+import org.junit.Test;
+
 public class FizSolutionTest {
+
+    private final FizzBuzzSolution fizzBuzzSolution = new FizzBuzzSolution();
+
+    @Test
+    public void shouldWriteFiz_whenNumberDivisableByThree() {
+
+        assertThat(fizzBuzzSolution.fizzBuzz(9),is("fizz"));
+    }
+
+
 }
