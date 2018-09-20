dbdcb99b6670a8c48457e69945939488e1fb3807
sbx_user1059 <sbx_user1059@ip-10-129-141-175.eu-west-2.compute.internal>
2018-09-20 12:03:41 +0000

    Thu Sep 20 12:03:41 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -11,30 +11,50 @@ public class FizzBuzzSolution {
         Boolean isMod5 = numberIsDivisibleBy(number, 5 );
 
         if((isMod3 || hasDigit3) && (isMod5 || hasDigit5)) {
-            if(numberIsDivisibleBy(number,2)) {
+            if (numberIsDivisibleBy(number, 2)) {
                 return "fizz buzz deluxe";
-            }else {
+            } else {
                 return "fizz buzz fake deluxe";
             }
         }
+        String fizz1 = getDivideBy3(number, hasDigit3, isMod3);
+        if (fizz1 != null) return fizz1;
 
-        if(isMod3 && hasDigit3) {
-            if(numberIsDivisibleBy(number,2)) {
-                return "fizz deluxe";
-            }else {
-                return "fizz fake deluxe";
-            }
-        }
-
-        if(isMod5 && hasDigit5) {
-            if(numberIsDivisibleBy(number,2)){
-                return "buzz deluxe";
-            }else {
-                return "buzz fake deluxe";
-            }
-        }
+        String buzz1 = getDivideBy5(number, hasDigit5, isMod5);
+        if (buzz1 != null) return buzz1;
 
         return number.toString();
+    }
+
+    private String getDivideBy3(final Integer number, final boolean hasDigit3, final Boolean isMod3) {
+        String fizz = "fizz";
+        if(isMod3 && hasDigit3) {
+
+            if(numberIsDivisibleBy(number,2)) {
+                fizz = fizz + " deluxe";
+            }else {
+                fizz = fizz +  " fake deluxe";
+            }
+            return fizz;
+        } else if(isMod3) {
+            return "fizz";
+        }
+        return null;
+    }
+
+    private String getDivideBy5(final Integer number, final boolean hasDigit5, final Boolean isMod5) {
+        String buzz = "buzz";
+        if(isMod5 && hasDigit5) {
+            if(numberIsDivisibleBy(number,2)){
+                buzz = buzz +  "deluxe";
+            }else {
+                buzz = buzz + " fake deluxe";
+            }
+            return buzz;
+        } else if(isMod5) {
+            return "buzz";
+        }
+        return null;
     }
 
 
