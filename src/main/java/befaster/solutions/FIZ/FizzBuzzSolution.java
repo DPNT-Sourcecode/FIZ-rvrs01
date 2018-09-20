fcb3962ad74d289697a72731f402076453c8eb62
sbx_user1059 <sbx_user1059@ip-10-129-141-175.eu-west-2.compute.internal>
2018-09-20 08:46:56 +0000

    Thu Sep 20 08:46:56 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -1,8 +1,6 @@
 package befaster.solutions.FIZ;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Predicate;
+import java.util.function.BiFunction;
 
 public class FizzBuzzSolution {
 
@@ -10,7 +8,6 @@ public class FizzBuzzSolution {
 
         boolean hasDigit3 = digitContainsCharacters("3", number);
         boolean hasDigit5 = digitContainsCharacters("5", number);
-
 
 
         if ((number % 3 == 0 || hasDigit3) && (number % 5 == 0 || hasDigit5)) {
@@ -27,23 +24,29 @@ public class FizzBuzzSolution {
     }
 
     private String numberGreaterThanTen(Integer number) {
-         if(number > 10 ? true : false) {
+        if (number > 10 && hasAllTheSameDigits(number)) {
             return "delux";
-         }
+        }
         return "";
     }
 
     private boolean hasAllTheSameDigits(Integer number) {
         String digits = number.toString();
-        String[] numbers = new String[digits.length()];
-        List<String> digitList = Arrays.asList(numbers);
+        Integer[] numbers = new Integer[digits.length()];
 
-     //   Predicate<String> digitEquality  {if (a == b) return true;};
-//        digitList.forEach(digit -> {
-//
-//        });
+        BiFunction<Integer, Integer, Integer> compareDigits = (a, b) -> a.compareTo(b);
 
-
+        if (compareDigits.apply(numbers[0], numbers[1]) == 0) {
+            if (numbers.length > 2) {
+                if (compareDigits.apply(numbers[1], numbers[2]) == 0) {
+                    if (numbers.length > 3) {
+                        if(compareDigits.apply(numbers[1], numbers[3]) == 0){
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
         return false;
     }
 
