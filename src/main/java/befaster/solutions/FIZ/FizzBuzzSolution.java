1a9b6a03144e2f4949b2f90ece3040b4406f85aa
sbx_user1060 <sbx_user1060@ip-10-129-9-125.eu-west-2.compute.internal>
2018-10-05 07:46:11 +0000

    Fri Oct 05 07:46:11 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -1,61 +1,11 @@
 package befaster.solutions.FIZ;
 
+import befaster.runner.SolutionNotImplementedException;
+
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
-
-        boolean hasDigit3 = digitContainsCharacters("3", number);
-        boolean hasDigit5 = digitContainsCharacters("5", number);
-
-        Boolean isMod3 = numberIsDivisibleBy(number, 3 );
-        Boolean isMod5 = numberIsDivisibleBy(number, 5 );
-
-        if((isMod3 || hasDigit3) && (isMod5 || hasDigit5)) {
-
-            if(isMod3 || isMod5)) {
-                return "fizz buzz";
-            }
-            else if (numberIsDivisibleBy(number, 2)) {
-                return "fizz buzz deluxe";
-            }
-             else {
-                return "fizz buzz fake deluxe";
-            }
-        }
-        String fizz1 = geKeywordDividedByDigitAndContainsDigit(number, hasDigit3, isMod3, "fizz");
-        if (fizz1 != null) return fizz1;
-
-        String buzz1 = geKeywordDividedByDigitAndContainsDigit(number, hasDigit5, isMod5, "buzz");
-        if (buzz1 != null) return buzz1;
-
-        return number.toString();
+        throw new SolutionNotImplementedException();
     }
 
-    private String geKeywordDividedByDigitAndContainsDigit(final Integer number, final boolean hasDigit, final Boolean isModNumber, String prefix) {
-
-        if(isModNumber && hasDigit) {
-            if(numberIsDivisibleBy(number,2)){
-                prefix = prefix +  " deluxe";
-                return prefix;
-            }else {
-                prefix = prefix+ " fake deluxe";
-                return prefix;
-            }
-        } else if(isModNumber || hasDigit) {
-            return prefix;
-        }
-        return null;
-    }
-
-
-    private boolean digitContainsCharacters(String str, Integer number) {
-        return number.toString().contains(str);
-    }
-
-    private boolean numberIsDivisibleBy( Integer number, int diviser) {
-       if (number % diviser == 0) {
-        return true;
-       }
-       return false;
-   }
 }
--- src/main/java/befaster/solutions/HLO/HelloSolution.java
+++ src/main/java/befaster/solutions/HLO/HelloSolution.java
@@ -1,11 +1,9 @@
 package befaster.solutions.HLO;
 
-import static java.lang.String.format;
-
 import befaster.runner.SolutionNotImplementedException;
 
 public class HelloSolution {
     public String hello(String friendName) {
-        return format("Hello, %s!",friendName);
+        throw new SolutionNotImplementedException();
     }
 }
