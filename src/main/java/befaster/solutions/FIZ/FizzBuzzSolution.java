0f01e50f0608f25dee7ea38c593ab3d65bcb5f7c
sbx_user1060 <sbx_user1060@ip-10-129-9-125.eu-west-2.compute.internal>
2018-10-05 09:59:31 +0000

    Fri Oct 05 09:59:31 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -2,30 +2,83 @@ package befaster.solutions.FIZ;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class FizzBuzzSolution {
 
     public String fizzBuzz(final int number) {
-        String numberValue = String.valueOf(number);
-        StringBuilder builder = new StringBuilder();
-        if(number % 3 == 0) {
-            builder.append("fizz");
-        }
-        if(number % 5 == 0) {
-            if(builder.length() != 0 ) {
-                builder.append(" ");
-            }
-            builder.append("buzz");
+        FizzBuzzStringBuilder builder = new FizzBuzzStringBuilder(number);
+        return builder.process();
+    }
+
+    public class FizzBuzzStringBuilder {
+
+        private Integer number;
+
+        FizzBuzzStringBuilder(int number) {
+            this.number = number;
         }
 
-        if(builder.length() != 0) {
-            if(numberValue.contains("3") || numberValue.contains("5")) {
-                if(number % 2 == 0) {
-                    return builder.append(" deluxe").toString();
-                }
-                return builder.append(" fake deluxe").toString();
+        String process() {
+            List<Optional<FizzBuzzIdentifier>> fizzBuzzIdentifierList = new ArrayList<>();
+            fizzBuzzIdentifierList.add(extract(3, "fizz"));
+            fizzBuzzIdentifierList.add(extract(5, "buzz"));
+
+            StringBuilder builder = new StringBuilder();
+            for(Optional<FizzBuzzIdentifier> fizzBuzzIdentifier : fizzBuzzIdentifierList) {
+                append(builder, fizzBuzzIdentifier);
             }
+            builder.append(extractDeluxe(fizzBuzzIdentifierList));
             return builder.toString();
         }
-        return numberValue;
+
+        String extractDeluxe(List<Optional<FizzBuzzIdentifier>> fizzBuzzIdentifierList) {
+            for(Optional<FizzBuzzIdentifier> fizzBuzzIdentifier : fizzBuzzIdentifierList) {
+                if(fizzBuzzIdentifier.isPresent()) {
+                    if(fizzBuzzIdentifier.get().isDivisible) {
+                        if(number.toString().contains("3") || number.toString().contains("5")) {
+                            if(number % 2 == 0) {
+                                return "deluxe";
+                            }
+                            return "fake deluxe";
+                        }
+                    }
+                }
+            }
+            return "";
+        }
+
+        void append(final StringBuilder builder, final Optional<FizzBuzzIdentifier> value) {
+            if(!value.isPresent()) {
+                return;
+            }
+
+            if(builder.length() > 0) {
+                builder.append(" ");
+            }
+            builder.append(value.get().type);
+        }
+
+        private Optional<FizzBuzzIdentifier> extract(Integer compareTo, String type) {
+            if(number % compareTo == 0) {
+                return Optional.of(new FizzBuzzIdentifier(type, true));
+            }
+            if(String.valueOf(number).contains(compareTo.toString())) {
+                return Optional.of(new FizzBuzzIdentifier(type, false));
+            }
+            return Optional.empty();
+        }
+    }
+
+    private class FizzBuzzIdentifier {
+        private String type;
+        private boolean isDivisible;
+
+        public FizzBuzzIdentifier(String type, boolean isDivisible) {
+            this.type = type;
+            this.isDivisible = isDivisible;
+        }
     }
 }
